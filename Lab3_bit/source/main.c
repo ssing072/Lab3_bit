658224e198f58ed27941ed35ff1feeb40e9d48a7
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-10-07 10:35:32 -0700

    Update main.c

--- Lab3_bit/source/main.c
+++ Lab3_bit/source/main.c
@@ -12,12 +12,35 @@
 #include "simAVRHeader.h"
 #endif
 
+unsigned char GetBit(unsigned char x, unsigned char k){
+    return ((x & (0x01 << k)) != 0);
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0x00; PORTB = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    
+    unsigned char i;
+    unsigned char cnt;
+    unsigned char A = 0x00;
+    unsigned char B = 0x00;
 
     /* Insert your solution below */
     while (1) {
-
+        A = PINA;
+        B = PINB;
+        cnt = 0;
+        for(i = 0; i < 8; i++){
+            if(GetBit(A, i)){
+                cnt++;
+            }
+            if(GetBit(B, i)){
+                cnt++;
+            }
+        }
+    PORTC = cnt;
     }
     return 1;
 }
