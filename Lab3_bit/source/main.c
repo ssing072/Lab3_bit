50154a9485de83c816f200709745f08cf5d21450
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-10-07 19:15:54 -0700

    Update main.c

--- Lab3_bit/source/main.c
+++ Lab3_bit/source/main.c
@@ -23,22 +23,22 @@ int main(void) {
     while(1){
         tempA = PINA;
         
-        if(tempA == 0x01 || tempA = 0x02){
+        if(tempA == 0x01 || tempA == 0x02){
             C = 0x20;
         }
-        else if(tempA == 0x03 || tempA = 0x04){
+        else if(tempA == 0x03 || tempA == 0x04){
             C = 0x30;
         }
-        else if(tempA == 0x05 || tempA = 0x06){
+        else if(tempA == 0x05 || tempA == 0x06){
             C = 0x38;
         }
-        else if(tempA == 0x07 || tempA = 0x08 || tempA = 0x09){
+        else if(tempA == 0x07 || tempA == 0x08 || tempA == 0x09){
             C = 0x3C;
         }
-        else if(tempA == 0x0A || tempA = 0x0B || tempA = 0x0C){
+        else if(tempA == 0x0A || tempA == 0x0B || tempA == 0x0C){
             C = 0x3E;
         }
-        else if(tempA == 0x0D || tempA = 0x0E || tempA = 0x0F){
+        else if(tempA == 0x0D || tempA == 0x0E || tempA == 0x0F){
             C = 0x3F;
         }
         PORTC = C;
