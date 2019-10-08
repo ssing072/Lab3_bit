863ed66ff8ee3cb845c8d9a48f9caabcd6c46fe6
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-10-07 21:26:07 -0700

    Update main.c

--- Lab3_bit/source/main.c
+++ Lab3_bit/source/main.c
@@ -14,16 +14,24 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-    DDRA = 0x00; PORTA = 0xFF;
-    DDRB = 0xFF; PORTB = 0x00;
-    DDRC = 0xFF; PORTC = 0x00;
+    DDRB = 0xFE; PORTB = 0x00;
+    DDRD = 0x00; PORTD = 0x00;
     
-    unsigned char tempA = 0x00;
+    unsigned long personWeight = 0;
+    unsigned char tempB = 0x00;
     
     while(1){
-        
-        PORTB = (PINA >> 4) | 0x0;
-        PORTC = (PINA << 4);
+        personWeight = PORTD + PORTB;
+        if(personWeight >= 70){
+            tempB = 0x02;
+        }
+        else if(personWeight <= 5){
+            tempB = 0x00;
+        }
+        else{
+            tempB = 0x04;
+        }
+        PORTB = tempB;
     }
     return 1;
 }
