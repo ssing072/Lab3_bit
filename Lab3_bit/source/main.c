31dd0aab5333b98adba930a83576b229415fd53a
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-10-08 15:13:20 -0700

    Update main.c

--- Lab3_bit/source/main.c
+++ Lab3_bit/source/main.c
@@ -1,70 +1,53 @@
-/* Author: magui051
-
-* Partner(s) Name:
-
-* Lab Section:
-
-* Assignment: Lab # Exercise #
-
-* Exercise Description: [optional - include for your own benefit]
-
-*
-
-* I acknowledge all content contained herein, excluding template or example
-
-* code, is my own original work.
-
-*/
-
+  
+/*	Author: ssing072
+ *  Partner(s) Name: Neha Gupta
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
 #include <avr/io.h>
-
 #ifdef _SIMULATE_
-
 #include "simAVRHeader.h"
-
 #endif
 
-unsigned char set(unsigned char pin, unsigned char bit_pos, unsigned char bit_val) {
-
-return (bit_val ? pin | (0x00 << bit_pos) : pin & ~(0x00 << bit_pos));
-
-}
-
-unsigned char get(unsigned char port, unsigned char bit_pos) {
-
-return ((port >> bit_pos) & 0x00);
-
-}
-
 int main(void) {
-
-/* Insert DDR and PORT initializations */
-
-DDRA = 0x00; PORTA = 0xFF; //input
-
-DDRB = 0x00; PORTB = 0xFF; //input
-
-DDRC = 0xFF; PORTC = 0x00; //output
-
-unsigned char cnt = 0x00;
-
-unsigned char tmpA = 0x00;
-
-unsigned char tmpB = 0x00;
-
-unsigned char loc = 0x00;
-
-while(1) {
-    tmpA = PINA;
-    tmpB = PINB;
-    for(loc = 0; loc < 8; loc++) {
-    cnt += get(tmpA, loc) + get(tmpB, loc);      
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    
+    unsigned char tempA = 0x00;
+    unsigned char tempB = 0x00;
+    unsigned char C = 0x00;
+    
+    while(1){
+        tempA = PINA & 0x0F;
+        tempB = PINA & 0xF0;
+        
+        if(tempA == 0x01 || tempA == 0x02){
+            C = 0x60;
+        }
+        else if(tempA == 0x03 || tempA == 0x04){
+            C = 0x70;
+        }
+        else if(tempA == 0x05 || tempA == 0x06){
+            C = 0x38;
+        }
+        else if(tempA == 0x07 || tempA == 0x08 || tempA == 0x09){
+            C = 0x3C;
+        }
+        else if(tempA == 0x0A || tempA == 0x0B || tempA == 0x0C){
+            C = 0x3E;
+        }
+        else if(tempA == 0x0D || tempA == 0x0E || tempA == 0x0F){
+            C = 0x3F;
+        }
+        if((PINA >> 4) == 0x03){
+             C = C | 0x80;  
+        }
+        PORTC = C;
     }
-    PORTC = cnt;
-    cnt = 0;
+    return 1;
 }
-return 1;
-
-}
-
-/* Insert your solution below */
